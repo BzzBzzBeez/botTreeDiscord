c498a7fb4be38c633a80b910aee8063d6580982a
BzzBzzBeez <55196216+BzzBzzBeez@users.noreply.github.com>
2023-06-07 17:34:37 +0200

    Create botTree.py

---
+++ botTree.py
@@ -0,0 +1,162 @@
+from time import sleep
+import pyautogui
+import cv2
+
+
+def isReady():
+    coords = pyautogui.locateCenterOnScreen('ready.png',confidence=0.8)
+    if coords != None: 
+        return True
+    else:
+        return False
+
+def clickWater():
+        coords = pyautogui.locateCenterOnScreen('watered.png',confidence=0.9, grayscale=False)
+        if coords != None: 
+            pyautogui.moveTo(coords.x, coords.y)
+            pyautogui.click()
+            pyautogui.moveTo(100,100)
+
+def alreadyWatered():
+        coords = pyautogui.locateCenterOnScreen('alreadyWatered.png',confidence=0.8)
+        if coords != None: 
+            return True
+        else:
+            return False
+
+def rejeter():
+        coords = pyautogui.locateCenterOnScreen('rejeter.png',confidence=0.8)
+        if coords != None: 
+
+            pyautogui.moveTo(coords.x, coords.y)
+            pyautogui.click()
+            pyautogui.moveTo(100,100)
+
+def rejeter2():
+        coords = pyautogui.locateCenterOnScreen('rejeter2.png',confidence=0.8)
+        if coords != None: 
+
+            pyautogui.moveTo(coords.x, coords.y)
+            pyautogui.click()
+            pyautogui.moveTo(100,100)
+
+def changeAccountTo1():
+    pyautogui.moveTo(175, 1018)
+    pyautogui.click()
+    sleep(.5)
+
+    coords = pyautogui.locateCenterOnScreen('change_account.png',confidence=0.8) # Change account
+    if coords != None: 
+
+        pyautogui.moveTo(coords.x, coords.y)
+        sleep(.5)
+
+        pyautogui.moveTo(406,947)
+        pyautogui.click()
+
+def changeAccountTo0():
+    pyautogui.moveTo(175, 1018)
+    pyautogui.click()
+    sleep(.5)
+
+    coords = pyautogui.locateCenterOnScreen('change_account.png',confidence=0.8) # Change account
+    if coords != None: 
+
+        pyautogui.moveTo(coords.x, coords.y)
+        sleep(.5)
+
+        pyautogui.moveTo(406,910)
+        pyautogui.click()
+
+def goToTreeChannel():
+    coords = pyautogui.locateCenterOnScreen('coin.png',confidence=0.8) # Go To COIN
+    if coords != None: 
+
+        pyautogui.moveTo(coords.x, coords.y)
+        pyautogui.click()
+        sleep(.5)
+
+        coords = pyautogui.locateCenterOnScreen('tree_channel.png',confidence=0.8) # Go To tree channel
+        if coords != None: 
+
+            pyautogui.moveTo(coords.x, coords.y)
+            pyautogui.click()
+            sleep(.5)
+
+def collectApples():
+    for coords in pyautogui.locateAllOnScreen('soup.png',confidence=0.9, grayscale=False): # Find soups
+        if coords != None: 
+            pyautogui.moveTo(coords[0]+30, coords[1]+20, 0.2)
+
+            pyautogui.click()
+            print("    Apple collected")
+            sleep(.2)
+#-------------------------------
+#AJOUTER CLIQUE COIN + CHANNEL
+
+print(pyautogui.position())
+print(pyautogui.size())
+sleep(1)
+i = 0 # Iteration
+a = 1 # Modulo % 2 / Pair impair
+
+#goToTreeChannel()
+
+while True:
+    goToTreeChannel()
+    if isReady():
+        print("Ready ! : ")
+        rejeter() #IF REJETER NOT DELETED
+        rejeter2() # IF LVL UP
+
+        clickWater()
+        print("Clicking")
+        sleep(.5)
+
+        if alreadyWatered():
+            rejeter()
+            print("  Rejecting...")
+            sleep(.5)
+
+            if a % 2 == 0:
+                changeAccountTo1()
+                sleep(5)
+                print("  Account Switched to 1")
+                goToTreeChannel()
+                print("   Go to COIN")
+            else:
+                changeAccountTo0()
+                sleep(5)
+                print("  Account Switched to 0")
+                goToTreeChannel()
+                print("  Go to COIN")
+            
+            a = a + 1 #Change accounts (a % 2)
+
+        else:
+            print("[Watered]")
+            sleep(5)
+            
+            print("  Apples checking")
+            collectApples()
+            
+            if a % 2 == 0:
+                changeAccountTo1()
+                sleep(5)
+                print("  Account Switched to 1")
+                goToTreeChannel()
+                print("  Go to COIN")
+            else:
+                changeAccountTo0()
+                sleep(5)
+                print("  Account Switched to 0")
+                goToTreeChannel()
+                print("  Go to COIN")
+            
+            a = a + 1 #Change accounts (a % 2)
+
+    else:
+        i = i+1 #Print Iteration
+        print(str(i))
+    
+    sleep(1)
